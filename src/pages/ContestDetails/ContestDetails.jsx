e652b1418935491a854dfa408447b658210285d6
himel2535 <monwarhossanhimel@gmail.com>
2025-12-08 00:44:23 +0600

    contest details page using tanstackQuery

--- src/pages/ContestDetails/ContestDetails.jsx
+++ src/pages/ContestDetails/ContestDetails.jsx
@@ -1,95 +1,116 @@
-import Container from '../../components/Shared/Container'
-import Heading from '../../components/Shared/Heading'
-import Button from '../../components/Shared/Button/Button'
-import PurchaseModal from '../../components/Modal/PurchaseModal'
-import { useState } from 'react'
+import Container from "../../components/Shared/Container";
+import Heading from "../../components/Shared/Heading";
+import Button from "../../components/Shared/Button/Button";
+import PurchaseModal from "../../components/Modal/PurchaseModal";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const ContestDetails = () => {
-  let [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const { id } = useParams();
 
-  const closeModal = () => {
-    setIsOpen(false)
-  }
+  const {
+    data: contest,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["contest", id],
+    queryFn: async () => {
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/contest/${id}`
+      );
+      return res.data;
+    },
+  });
+
+  const closeModal = () => setIsOpen(false);
+
+  if (isLoading) return <p className="text-center py-10">Loading...</p>;
+  if (isError) return <p className="text-center py-10">Error loading data</p>;
+
+  // destructuring contest data safely
+  const {
+    image,
+    name,
+    description,
+    participantsCount,
+    prizeMoney,
+    contestFee,
+    category,
+    contestCreator,
+  } = contest || {};
 
   return (
     <Container>
-      <div className='mx-auto flex flex-col lg:flex-row justify-between w-full gap-12'>
-        {/* Header */}
-        <div className='flex flex-col gap-6 flex-1'>
-          <div>
-            <div className='w-full overflow-hidden rounded-xl'>
-              <img
-                className='object-cover w-full'
-                src='https://i.ibb.co/DDnw6j9/1738597899-golden-money-plant.jpg'
-                alt='header image'
-              />
+      <div className="mx-auto flex flex-col lg:flex-row justify-between w-full gap-12">
+        {/* Left / Image */}
+        <div className="flex-1">
+          <img
+            className="rounded-xl h-full w-full object-cover"
+            src={image}
+            alt={name}
+          />
+        </div>
+
+        {/* Right / Details */}
+        <div className="flex-1">
+          <Heading title={name} subtitle={`Category: ${category}`} />
+
+          <hr className="my-4" />
+
+          <p className="text-neutral-600 leading-relaxed">{description}</p>
+
+          <hr className="my-4" />
+
+          <div className="flex items-center gap-3 mt-3">
+            {" "}
+            <img
+              src={contestCreator?.image}
+              alt="creator"
+              className="w-15 h-15 rounded-full"
+              referrerPolicy="no-referrer"
+            />
+            <div>
+              <p className="font-semibold text-neutral-500">{contestCreator?.name}</p>
+              <p className="font-semibold text-neutral-500">
+                {contestCreator?.email}
+              </p>
             </div>
           </div>
-        </div>
-        <div className='md:gap-10 flex-1'>
-          {/* Plant Info */}
-          <Heading
-            title={'Money Plant'}
-            subtitle={`Category: ${'Succulent'}`}
-          />
-          <hr className='my-6' />
-          <div
-            className='
-          text-lg font-light text-neutral-500'
-          >
-            Professionally deliver sticky testing procedures for next-generation
-            portals. Objectively communicate just in time infrastructures
-            before.
-          </div>
-          <hr className='my-6' />
 
-          <div
-            className='
-                text-xl 
-                font-semibold 
-                flex 
-                flex-row 
-                items-center
-                gap-2
-              '
-          >
-            <div>Seller: Shakil Ahmed Atik</div>
+          <hr className="my-4" />
 
-            <img
-              className='rounded-full'
-              height='30'
-              width='30'
-              alt='Avatar'
-              referrerPolicy='no-referrer'
-              src='https://lh3.googleusercontent.com/a/ACg8ocKUMU3XIX-JSUB80Gj_bYIWfYudpibgdwZE1xqmAGxHASgdvCZZ=s96-c'
-            />
-          </div>
-          <hr className='my-6' />
-          <div>
-            <p
-              className='
-                gap-4 
-                font-light
-                text-neutral-500
-              '
-            >
-              Quantity: 10 Units Left Only!
+          <p className="text-lg font-semibold">
+            Participants: {participantsCount}
+          </p>
+
+          <hr className="my-4" />
+
+          {/* Creator Info */}
+          <div className="flex items-center justify-between">
+            <p className="text-xl font-bold mt-2 text-gray-700">
+              Prize Money: ${prizeMoney}
+            </p>
+            <p className="text-xl font-bold mt-2 text-gray-700">
+              Registration Fee: ${contestFee}
             </p>
           </div>
-          <hr className='my-6' />
-          <div className='flex justify-between'>
-            <p className='font-bold text-3xl text-gray-500'>Price: 10$</p>
-            <div>
-              <Button onClick={() => setIsOpen(true)} label='Purchase' />
-            </div>
-          </div>
-          <hr className='my-6' />
 
-          <PurchaseModal closeModal={closeModal} isOpen={isOpen} />
+          <hr className="my-6" />
+
+          {/* Pay / Register */}
+          <div className="flex justify-between items-center">
+            <Button label="Pay" onClick={() => setIsOpen(true)} />
+          </div>
+
+          {/* Modal */}
+          <PurchaseModal isOpen={isOpen} closeModal={closeModal} />
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ContestDetails
+export default ContestDetails;
