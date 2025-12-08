8c944d67a369142a85b5c2c8212bac81a8702571
himel2535 <monwarhossanhimel@gmail.com>
2025-12-08 18:03:16 +0600

    post route create for orders collection

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import { useSearchParams } from "react-router";
+// import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect } from "react";
 
 const PaymentSuccess = () => {
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+
+ useEffect(() => {
+  if (sessionId) {
+    axios.post(`${import.meta.env.VITE_API_URL}/payment-success`, {
+      sessionId,
+    });
+  }
+}, [sessionId]);
+
+  // TanStack Query
+  // const {
+  //   data: payment,
+  //   isLoading,
+  //   isError,
+  // } = useQuery({
+  //   queryKey: ["paymentStatus", sessionId],
+  //   queryFn: async () => {
+  //     const res = await axios.get(
+  //       `${import.meta.env.VITE_API_URL}/payment/verify/${sessionId}`
+  //     );
+  //     return res.data;
+  //   },
+  //   enabled: !!sessionId,
+  // });
+
+  // if (isLoading) return <p>Loading payment status...</p>;
+  // if (isError) return <p>Something went wrong!</p>;
+
   return (
-    <div>
-      <h1>Successfully Payment done</h1>
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-green-600">
+        Payment Successful 🎉
+      </h1>
+
+      <p className="mt-4 text-lg">
+        Payment ID: <span className="font-semibold">
+          {/* {payment.id} */}
+
+          </span>
+      </p>
+      <p className="mt-4 text-lg">
+        Transaction ID:{" "}
+        <span className="font-semibold">
+          {/* {payment.payment_intent} */}
+          </span>
+      </p>
+
+      <p className="mt-2 text-lg">
+        Amount Paid:{" "}
+        <span className="font-semibold">
+          {/* ${payment.amount_total / 100} */}
+          
+          </span>
+      </p>
+
+      <p className="mt-2 text-md text-gray-600">
+        Status:
+         {/* {payment.payment_status} */}
+      </p>
     </div>
   );
 };
