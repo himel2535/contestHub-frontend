9d6f7a5b98f81d44d4efb41be1f8789efb737a9f
himel2535 <monwarhossanhimel@gmail.com>
2025-12-08 18:11:14 +0600

    create order collection in database and push data

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("session_id");
-
+// 
  useEffect(() => {
   if (sessionId) {
     axios.post(`${import.meta.env.VITE_API_URL}/payment-success`, {
